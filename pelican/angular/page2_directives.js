230a92128528fe4f920034952431f1c4c9a0df31
Charles Reid <charlesreid1@gmail.com>
2015-04-11 15:04:44 -0700

    updating angular directive appending example

--- pelican/angular/page2_directives.js
+++ pelican/angular/page2_directives.js
@@ -3,29 +3,36 @@
 /////////////////////////
 // Directive 
 
-var ngd1 = mod.directive("populatebutton", function(){
+var ngd0 = mod.directive("helloworld", function(){
     return {
         restrict: "E",
-        template: "<button class='btn btn-large btn-info' populate>Click to populate page 2</button>"
+        template: "<p><b>Hello cruel angular world! This is <code>helloworld</code> talking.</b></p>"
     }
 });
 
-//Directive for adding buttons on click that show an alert on click
-var ngd2 = mod.directive("populate", function($compile){
+
+var ngd1 = mod.directive("populate1", function($compile){
     return function(scope, element, attrs){
         element.bind("click", function(){
-            scope.count++;
-            //angular.element(document.getElementById('space-for-buttons')).append($compile("<div><button class='btn btn-large btn-info' data-alert="+scope.count+">Show alert #"+scope.count+"</button></div>")(scope));
-            angular.element(document.getElementById('go2')).append($compile("<div><h2>Hello world</h2><p>Now this page is populated.</p></div>")(scope));
+            angular.element(document.getElementById('go1')).empty();
+            angular.element(document.getElementById('go1')).append($compile("<div><h1>Hello world</h1><p>Now page 1 has been populated from a directive.</p><helloworld></helloworld></div>")(scope));
         });
     };
 });
 
-//Directive for showing an alert on click
-var ngd3 = mod.directive("alert", function(){
+var ngd2 = mod.directive("populate2", function($compile){
     return function(scope, element, attrs){
         element.bind("click", function(){
-            alert("This is alert #"+attrs.alert);
+            angular.element(document.getElementById('go2')).empty();
+            angular.element(document.getElementById('go2')).append($compile("<div><h1>Hello world</h1><p>Now page 2 has been populated from a directive.</p><helloworld></helloworld></div>")(scope));
+        });
+    };
+});
+var ngd3 = mod.directive("populate3", function($compile){
+    return function(scope, element, attrs){
+        element.bind("click", function(){
+            angular.element(document.getElementById('go3')).empty();
+            angular.element(document.getElementById('go3')).append($compile("<div><h1>Hello world</h1><p>Now page 3 has been populated from a directive.</p><helloworld></helloworld></div>")(scope));
         });
     };
 });
