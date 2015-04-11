e01128e05c1151ab327a8ccc1d8d3e90d40555cc
Charles Reid <charlesreid1@gmail.com>
2015-04-10 20:56:07 -0700

    updating with latest examples. tabbed navigation.

---
+++ page1/page1_module.js
@@ -0,0 +1,10 @@
+/////////////////////////
+// Module
+
+var mod = angular.module("myApp",[],function($interpolateProvider) {
+        $interpolateProvider.startSymbol('[[');
+        $interpolateProvider.endSymbol(']]');
+    }
+);
+
+var c = mod.controller("Ctrl1", ["$scope","$http","$interval",Ctrl1]);
