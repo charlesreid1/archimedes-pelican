27922fd00aecb6a19bf2d5557881414eb7e6afe8
Charles Reid <charlesreid1@gmail.com>
2015-04-17 15:40:22 -0700

    streamgraph has axes and is plotting correctly from the data, but we still need to fix the axis labels, and figure out what is pinching the graphs at this one particular point around 1980-1981

--- pelican/angular/tax/tax_directives_categoriesexplorer.js
+++ pelican/angular/tax/tax_directives_categoriesexplorer.js
@@ -299,7 +299,7 @@ var c_chart_dir = mod.directive('categoriesExplorerBar', function($compile) {
 // indv/corp streamgraph
 //
 
-var c_indcorp_dir = mod.directive('categoriesExplorerStreamgraph', function($compile) {
+var c_indcorp_dir = mod.directive('categoriesExplorerStreamgraph1', function($compile) {
     function link(scope, element, attr) {
         var pscope = scope.$parent;
         if( !pscope.taxData ) { 
@@ -326,6 +326,7 @@ var c_indcorp_dir = mod.directive('categoriesExplorerStreamgraph', function($com
                 .attr("id","totalyear")
                 .append('b')
                 .text('Streamgraph');
+
             var svg = d3.select(el).append("svg")
                 .attr("width", width)
                 .attr("height", height);
@@ -353,10 +354,6 @@ var c_indcorp_dir = mod.directive('categoriesExplorerStreamgraph', function($com
                 var xkey = 'year';
                 var ykeys = ['corp','indv'];
 
-                var color = d3.scale.ordinal()
-                    .domain(ykeys)
-                    .range(["steelblue", "orange"]);
-
                 // data_array is a pair of arrays, 
                 // containing time series vectors 
                 // of coordinates (x,y)
@@ -368,6 +365,7 @@ var c_indcorp_dir = mod.directive('categoriesExplorerStreamgraph', function($com
 
                 var stack = d3.layout.stack().offset("wiggle");
                 layers0 = stack(data_array);
+                console.log(layers0);
 
                 var x = d3.scale.linear()
                     .domain([
@@ -380,10 +378,14 @@ var c_indcorp_dir = mod.directive('categoriesExplorerStreamgraph', function($com
                     .domain([0, d3.max(layers0, function(layer) { return d3.max(layer, function(d) { return d.y0 + d.y; }); })])
                     .range([height, 0]);
                 
+                var z = d3.scale.ordinal()
+                    .domain(ykeys)
+                    .range(["steelblue", "orange"]);
+
                 var area = d3.svg.area()
-                    .x(function(d) { return x(d.x); })
+                    .x(function(d)  { console.log('-----'); console.log("x = "+x(d.x)); return x(d.x); })
                     .y0(function(d) { return y(d.y0); })
-                    .y1(function(d) { return y(d.y0 + d.y); });
+                    .y1(function(d) { console.log("y = "+y(d.y)); return y(d.y0 + d.y); });
                 
                 //
                 // i learned my lesson from the bar chart:
@@ -410,3 +412,221 @@ var c_indcorp_dir = mod.directive('categoriesExplorerStreamgraph', function($com
     };
 
 });
+
+var c_indcorp_dir = mod.directive('categoriesExplorerStreamgraph', function($compile) {
+    function link(scope, element, attr) {
+        var pscope = scope.$parent;
+        if( !pscope.taxData ) { 
+            pscope.$watch('taxData', chartCallback);
+        } else {
+            chartCallback();
+        }
+
+        function chartCallback() {
+
+            var margin = {top: 20, right: 40, bottom: 30, left: 30};
+            var width = 500 - margin.left - margin.right;
+            var height = 400 - margin.top - margin.bottom;
+            
+            var tooltip = d3.select("body")
+                .append("div")
+                .attr("class", "remove")
+                .style("position", "absolute")
+                .style("z-index", "20")
+                .style("visibility", "hidden")
+                .style("top", "30px")
+                .style("left", "55px");
+
+            var el = element[0];
+            var br = d3.select(el).append('br');
+            var h2 = d3.select(el).append('h2')
+                .attr("id","totalyear")
+                .append('b')
+                .text('Streamgraph');
+
+            var svg = d3.select(el).append("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+              .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+            ///////////////////////////////
+            var datearray = [];
+            var colorrange = [];
+            var format = d3.time.format("%m/%d/%y");
+            strokecolor = 'black';
+            ///////////////////////////////
+
+            
+            pscope.$watch('myfilter',chartUpdate);
+
+            function chartUpdate() {
+
+                taxData = pscope.taxData;
+                myfilter = pscope.myfilter;
+
+                catData = taxData.filter(function(d) { return d['omb_cat'] == myfilter; });
+                newdata = d3.nest()
+                    .key( function(d) { return d.year; } )
+                    .sortKeys(d3.ascending)
+                    .rollup( function(d) {
+                        return {
+                            total: d3.sum(d,function(g){return g.total}),
+                            corp:  d3.sum(d,function(g){return g.corp }),
+                            indv:  d3.sum(d,function(g){return g.indv })
+                        }
+                    })
+                    .entries(catData);
+
+                var xkey = 'year';
+                var ykeys = ['corp','indv'];
+
+                // data_array is a pair of arrays, 
+                // containing time series vectors 
+                // of coordinates (x,y)
+                var data_array = ykeys.map(function (k) {
+                    return newdata.map(function(e, i) { 
+                        return {x: +e.key, y: e.values[k]/1000000}; 
+                    })
+                });
+
+                var stack = d3.layout.stack().offset("silhouette");
+                layers0 = stack(data_array);
+
+                var x = d3.scale.linear()
+                    .range([0, width]);
+                
+                var y = d3.scale.linear()
+                    .range([height, 0]);
+                
+                var z = d3.scale.ordinal()
+                    .domain(ykeys)
+                    .range(["steelblue", "orange"]);
+
+                var xAxis = d3.svg.axis()
+                    .scale(x)
+                    .orient("bottom");
+
+                var yAxis = d3.svg.axis()
+                    .scale(y);
+                
+                var yAxisr = d3.svg.axis()
+                    .scale(y);
+
+                x.domain([
+                        d3.min( layers0, function(layer) { 
+                            return d3.min(layer, function(z) { return z.x; }); 
+                        }),
+                        d3.max( layers0, function(layer) { 
+                            return d3.max(layer, function(z) { return z.x; }); 
+                        })
+                ]);
+
+                y.domain([
+                        0, 
+                        d3.max(layers0, function(layer) { 
+                            return d3.max(layer, function(d) { return d.y0 + d.y; }) 
+                        })
+                ]);
+
+                var area = d3.svg.area()
+                    .x(function(d)  { return x(d.x); })
+                    .y0(function(d) { return y(d.y0); })
+                    .y1(function(d) { return y(d.y0 + d.y); });
+
+                svg.selectAll("path").remove();
+                svg.selectAll("path")
+                    .data(layers0)
+                    .enter()
+                    .append('path')
+                    .attr('d',area)
+                    .attr('fill',function(layer,i) { 
+                        return z(i)
+                    });
+
+                svg.append("g")
+                    .attr("class", "x axis")
+                    .attr("transform", "translate(0," + height + ")")
+                    .call(xAxis);
+                
+                svg.append("g")
+                    .attr("class", "y axis")
+                    .attr("transform", "translate(" + width + ", 0)")
+                    .call(yAxis.orient("right"));
+            
+                svg.append("g")
+                    .attr("class", "y axis")
+                    .call(yAxis.orient("left"));
+            
+                /*
+                    .on("mousemove", function(d, i) {
+                      mousex = d3.mouse(this);
+                      mousex = mousex[0];
+                      var invertedx = x.invert(mousex);
+                      invertedx = invertedx.getMonth() + invertedx.getDate();
+                      var selected = (d.values);
+                      for (var k = 0; k < selected.length; k++) {
+                        datearray[k] = selected[k].date
+                        datearray[k] = datearray[k].getMonth() + datearray[k].getDate();
+                      }
+            
+                      mousedate = datearray.indexOf(invertedx);
+                      pro = d.values[mousedate].value;
+            
+                      d3.select(this)
+                      .classed("hover", true)
+                      .attr("stroke", strokecolor)
+                      .attr("stroke-width", "0.5px"), 
+                      tooltip.html( "<p>" + d.key + "<br>" + pro + "</p>" ).style("visibility", "visible");
+                      
+                    })
+                    .on("mouseout", function(d, i) {
+                     svg.selectAll(".layer")
+                      .transition()
+                      .duration(250)
+                      .attr("opacity", "1");
+                      d3.select(this)
+                      .classed("hover", false)
+                      .attr("stroke-width", "0px"), tooltip.html( "<p>" + d.key + "<br>" + pro + "</p>" ).style("visibility", "hidden");
+                })
+                  
+                var vertical = d3.select(".chart")
+                      .append("div")
+                      .attr("class", "remove")
+                      .style("position", "absolute")
+                      .style("z-index", "19")
+                      .style("width", "1px")
+                      .style("height", "380px")
+                      .style("top", "10px")
+                      .style("bottom", "30px")
+                      .style("left", "0px")
+                      .style("background", "#fff");
+            
+                d3.select(".chart")
+                    .on("mousemove", function(){  
+                       mousex = d3.mouse(this);
+                       mousex = mousex[0] + 5;
+                       vertical.style("left", mousex + "px" )})
+                    .on("mouseover", function(){  
+                       mousex = d3.mouse(this);
+                       mousex = mousex[0] + 5;
+                       vertical.style("left", mousex + "px")});
+                });
+                */
+            };
+        };
+    };
+
+    return {
+        link: link,
+        restrict: "E",
+        scope: {
+            myfilter : '=',
+            taxData : '='
+        }
+    };
+
+});
+
+
